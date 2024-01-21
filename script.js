e90e08248f598c79b03346b108eaf49b26df2b4b
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-01-21 12:23:26 +0530

    Add:added the lap time feature

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const START = document.querySelector("#start");
 const PAUSE = document.querySelector("#pause");
 const RESET =  document.querySelector("#reset");
+const LAP = document.querySelector("#lap");
+const LAP_DATA = document.querySelector("#lap-data")
 
 let timeInMs = 0;
+let ms = 0;
+let sec = 0;
+let min = 0;
 let timer;
 
 /***************************************************************************
  updateTimer fucntion will update the DOM according to the value calculated  form the timeInMs variable
+ @returns none
 ***************************************************************************/
 
 function updateTimer(){
-    let sec = Math.floor(timeInMs / 1000) % 60;
-    let min = Math.floor(timeInMs / (60 * 1000)) % 60;
-    let ms = Math.floor(timeInMs % 1000);
+    sec = Math.floor(timeInMs / 1000) % 60;
+    min = Math.floor(timeInMs / (60 * 1000)) % 60;
+    ms = Math.floor(timeInMs % 1000);
 
     document.querySelector("#sec").innerText = sec < 10 ? "0" + sec : sec;
     document.querySelector("#min").innerText = min < 10 ? "0" + min : min;
@@ -21,6 +27,7 @@ function updateTimer(){
 
 /**************************************************************************
   startTimer fucntion will start the timer and update timeInMs variable every 10ms and call the updateTimer function to update the DOM
+  @returns none
 **************************************************************************/
 function startTimer(){
     timer = setInterval(() => {
@@ -35,6 +42,7 @@ function startTimer(){
 
 /**************************************************************************
  pauseTimer function will pause the timer
+ @returns none
 **************************************************************************/
 function pauseTimer(){
     clearInterval(timer);
@@ -45,6 +53,7 @@ function pauseTimer(){
 
 /**************************************************************************
  resetTimer function will reset the timer value to its initial state
+ @returns none
 **************************************************************************/
 
 function resetTimer(){
@@ -56,8 +65,53 @@ function resetTimer(){
     START.classList.remove("btn-disabled");
 }
 
+/**************************************************************************
+ lapTimer function will create a lap time and display it in UI whenever the lap switch is pressed.
+ @returns none
+***************************************************************************/
+
+function lapTimer(){
+
+    LAP_DATA.classList.remove("hidden");
+
+    let div = document.createElement("div");
+
+    div.innerHTML = `
+        <p><span class="lap-min">${min < 10 ? "0" + min : min}</span>: <span id="lap-sec">${sec < 10 ? "0" + sec : sec}</span>: <span id="lap-ms">${ms/10 < 10 ? "0" + ms/10 : ms/10}</span></p>
+                
+        <p>X</p>
+    `
+
+    div.classList.add("lap-card")
+    LAP_DATA.appendChild(div)
+}
+
+/**************************************************************************
+ deleteLap function will delete the lap card when X is pressed
+ @param {Object} e - event data of the particular event
+ @returns none
+***************************************************************************/
+
+function deleteLap(e){
+    if(e.target.innerText === "X"){
+        if(e.target.parentNode.parentNode.children.length === 1){
+            LAP_DATA.classList.add("hidden");
+        }
+        e.target.parentNode.remove()
+    }
+}
+
+// eveny listener for starting the timer
 START.addEventListener("click",startTimer);
 
+// event listener to pause the timer
 PAUSE.addEventListener("click",pauseTimer);
 
+// event listener to reset the timer to zero
 RESET.addEventListener("click",resetTimer);
+
+// event listener to create lap time
+LAP.addEventListener("click",lapTimer);
+
+// evnet listener to delete lap cards - uses event delegation
+LAP_DATA.addEventListener("click",deleteLap);
